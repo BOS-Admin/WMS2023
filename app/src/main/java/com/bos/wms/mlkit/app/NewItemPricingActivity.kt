5099327a0aeac9d8598b700e345dd4f11defbba3
m.othman <mhmd01ali01@gmail.com>
2023-08-10 14:07:08 +0300

    fix input UPCA and IS in new Item Pricing

--- app/src/main/java/com/bos/wms/mlkit/app/NewItemPricingActivity.kt
+++ app/src/main/java/com/bos/wms/mlkit/app/NewItemPricingActivity.kt
@@ -151,6 +151,10 @@ class NewItemPricingActivity : AppCompatActivity() {
                     return;
                 UpdatingText=true;
                 var itemSerialStr:String=txtItemSerial.text.toString()
+
+                if(isValidUPCA(itemSerialStr))
+                    itemSerialStr = convertToIS(itemSerialStr)
+
                 lblError.text = ""
 
                 if(!General.ValidateItemSerialCode(itemSerialStr)){
@@ -200,6 +204,33 @@ class NewItemPricingActivity : AppCompatActivity() {
     private fun Beep(){
         ToneGenerator(AudioManager.STREAM_MUSIC, ToneGenerator.MAX_VOLUME).startTone(ToneGenerator.TONE_SUP_ERROR, 300)
     }
+
+    //region UPCA
+    fun calculateUPCAChecksum(barcodeWithoutChecksum: String): Int {
+        val reversed = barcodeWithoutChecksum.reversed().toCharArray()
+        val sum = (0 until reversed.size).sumOf { i ->
+            Character.getNumericValue(reversed[i]) * if (i % 2 == 0) 3 else 1
+        }
+        return (10 - sum % 10) % 10
+    }
+
+
+    fun convertToIS(upca: String): String {
+        return "IS00" + upca.substring(2, upca.length - 1)
+    }
+    fun isValidUPCA(barcode: String): Boolean {
+        if (barcode.length != 12 || !barcode.startsWith("22")) {
+            return false
+        }
+
+        val checksumDigit = Character.getNumericValue(barcode[11])
+        val barcodeWithoutChecksum = barcode.substring(0, 11)
+        val expectedChecksum = calculateUPCAChecksum(barcodeWithoutChecksum)
+
+        return checksumDigit == expectedChecksum
+    }
+    //endregion
+
     fun ValidateScan(ItemSerial:String) {
 
         try {
